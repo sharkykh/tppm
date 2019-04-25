f8c23185aacac39ca4fdc6117a4b00f8e1297976
sharkykh <sharkykh@gmail.com>
2019-04-25 22:57:54 +0300

    Change playback item's template

--- src/components/playback-item.vue
+++ src/components/playback-item.vue
@@ -1,11 +1,14 @@
 <template>
   <sui-card>
-    <sui-card-content
-      v-if="info.type === 'episode'"
-      class="left aligned"
-    >
-      <sui-card-header>{{ info.show.title }}</sui-card-header>
-      <sui-card-meta>
+    <sui-card-content class="left aligned">
+      <sui-card-header v-if="isEpisode">
+        {{ info.show.title }}
+      </sui-card-header>
+      <sui-card-header v-else-if="isMovie">
+        {{ info.movie.title }}
+      </sui-card-header>
+
+      <sui-card-meta v-if="isEpisode">
         <sui-icon
           name="tv"
           title="Episode"
@@ -18,28 +21,19 @@
         />
         {{ info.episode.number }}
       </sui-card-meta>
-      <sui-card-meta>
-        <sui-icon name="pencil alternate" />
-        {{ info.episode.title }}
-      </sui-card-meta>
-      <sui-card-meta>
-        <sui-icon name="pause" />
-        {{ date }}
-      </sui-card-meta>
-    </sui-card-content>
-
-    <sui-card-content
-      v-else-if="info.type === 'movie'"
-      class="left aligned"
-    >
-      <sui-card-header>{{ info.movie.title }}</sui-card-header>
-      <sui-card-meta>
+      <sui-card-meta v-else-if="isMovie">
         <sui-icon
           name="film"
           title="Movie"
         />
         {{ info.movie.year }}
       </sui-card-meta>
+
+      <sui-card-meta v-if="isEpisode">
+        <sui-icon name="pencil alternate" />
+        {{ info.episode.title }}
+      </sui-card-meta>
+
       <sui-card-meta>
         <sui-icon name="pause" />
         {{ date }}
@@ -82,6 +76,12 @@ export default {
     },
   },
   computed: {
+    isEpisode() {
+      return this.info.type === 'episode';
+    },
+    isMovie() {
+      return this.info.type === 'movie';
+    },
     date() {
       const date = Date.parse(this.info.paused_at);
       const Z = new Date(date).getTimezoneOffset() ? 'Z' : '';
