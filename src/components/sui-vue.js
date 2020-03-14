b78e6e2693a1415bb5740cd337d6d047be138096
sharkykh <sharkykh@gmail.com>
2020-03-14 16:48:55 +0200

    Fix Sui-Vue Imports

--- src/components/sui-vue.js
+++ src/components/sui-vue.js
@@ -4,86 +4,60 @@ import 'semantic-ui-css/components/site.min.css';
 import 'semantic-ui-css/components/transition.min.css';
 
 // Message
-export {
-  Message as SuiMessage,
-} from 'semantic-ui-vue/dist/commonjs/collections/Message';
+export { default as SuiMessage } from 'semantic-ui-vue/dist/commonjs/collections/Message/Message';
 import 'semantic-ui-css/components/message.min.css';
 
 // Card
-export {
-  Card as SuiCard,
-  CardContent as SuiCardContent,
-  CardHeader as SuiCardHeader,
-  CardMeta as SuiCardMeta,
-  CardGroup as SuiCardGroup,
-} from 'semantic-ui-vue/dist/commonjs/views/Card';
+export { default as SuiCard } from 'semantic-ui-vue/dist/commonjs/views/Card/Card';
+export { default as SuiCardContent } from 'semantic-ui-vue/dist/commonjs/views/Card/CardContent';
+export { default as SuiCardHeader } from 'semantic-ui-vue/dist/commonjs/views/Card/CardHeader';
+export { default as SuiCardMeta } from 'semantic-ui-vue/dist/commonjs/views/Card/CardMeta';
+export { default as SuiCardGroup } from 'semantic-ui-vue/dist/commonjs/views/Card/CardGroup';
 import 'semantic-ui-css/components/card.min.css';
 
 // Button
-export {
-  Button as SuiButton,
-} from 'semantic-ui-vue/dist/commonjs/elements/Button';
+export { default as SuiButton } from 'semantic-ui-vue/dist/commonjs/elements/Button/Button';
 import 'semantic-ui-css/components/button.min.css';
 
 // Icon
-export {
-  Icon as SuiIcon,
-} from 'semantic-ui-vue/dist/commonjs/elements/Icon';
+export { default as SuiIcon } from 'semantic-ui-vue/dist/commonjs/elements/Icon/Icon';
 import 'semantic-ui-css/components/icon.min.css';
 
 // Progress
-export {
-  Progress as SuiProgress,
-} from 'semantic-ui-vue/dist/commonjs/modules/Progress';
+export { default as SuiProgress } from 'semantic-ui-vue/dist/commonjs/modules/Progress/Progress';
 import 'semantic-ui-css/components/progress.min.css';
 
 // Divider
-export {
-  Divider as SuiDivider,
-} from 'semantic-ui-vue/dist/commonjs/elements/Divider';
+export { default as SuiDivider } from 'semantic-ui-vue/dist/commonjs/elements/Divider/Divider';
 import 'semantic-ui-css/components/divider.min.css';
 
 // Container
-export {
-  Container as SuiContainer,
-} from 'semantic-ui-vue/dist/commonjs/elements/Container';
+export { default as SuiContainer } from 'semantic-ui-vue/dist/commonjs/elements/Container/Container';
 import 'semantic-ui-css/components/container.min.css';
 
 // Header
-export {
-  Header as SuiHeader,
-  HeaderContent as SuiHeaderContent,
-} from 'semantic-ui-vue/dist/commonjs/elements/Header';
+export { default as SuiHeader } from 'semantic-ui-vue/dist/commonjs/elements/Header/Header';
+export { default as SuiHeaderContent } from 'semantic-ui-vue/dist/commonjs/elements/Header/HeaderContent';
 import 'semantic-ui-css/components/header.min.css';
 
 // Image
-export {
-  Image as SuiImage,
-} from 'semantic-ui-vue/dist/commonjs/elements/Image';
+export { default as SuiImage } from 'semantic-ui-vue/dist/commonjs/elements/Image/Image';
 import 'semantic-ui-css/components/image.min.css';
 
 // Label
-export {
-  Label as SuiLabel,
-} from 'semantic-ui-vue/dist/commonjs/elements/Label';
+export { default as SuiLabel } from 'semantic-ui-vue/dist/commonjs/elements/Label/Label';
 import 'semantic-ui-css/components/label.min.css';
 
 // Loader
-export {
-  Loader as SuiLoader,
-} from 'semantic-ui-vue/dist/commonjs/elements/Loader';
+export { default as SuiLoader } from 'semantic-ui-vue/dist/commonjs/elements/Loader/Loader';
 import 'semantic-ui-css/components/loader.min.css';
 
 // Segment
-export {
-  Segment as SuiSegment,
-} from 'semantic-ui-vue/dist/commonjs/elements/Segment';
+export { default as SuiSegment } from 'semantic-ui-vue/dist/commonjs/elements/Segment/Segment';
 import 'semantic-ui-css/components/segment.min.css';
 
 // Dropdown
-export {
-  Dropdown as SuiDropdown,
-  DropdownMenu as SuiDropdownMenu,
-  DropdownItem as SuiDropdownItem,
-} from 'semantic-ui-vue/dist/commonjs/modules/Dropdown';
+export { default as SuiDropdown } from 'semantic-ui-vue/dist/commonjs/modules/Dropdown/Dropdown';
+export { default as SuiDropdownMenu } from 'semantic-ui-vue/dist/commonjs/modules/Dropdown/DropdownMenu';
+export { default as SuiDropdownItem } from 'semantic-ui-vue/dist/commonjs/modules/Dropdown/DropdownItem';
 import 'semantic-ui-css/components/dropdown.min.css';
